023b4705b29741a43b4a31e6b4030ca6809a882d
Shuangyue Cheng <kat@Shuangyues-MacBook-Pro.local>
2022-02-22 18:31:50 -0600

    add page connections

--- VIPapplication/View/RequestLocationNotifView.swift
+++ VIPapplication/View/RequestLocationNotifView.swift
@@ -9,47 +9,54 @@ import SwiftUI
 
 struct RequestLocationNotifView: View {
     var body: some View {
-        ZStack {
-            Color.white.edgesIgnoringSafeArea(.all)
-            VStack {
-                Image("NotifTop image")
-                    .resizable()
-                    .frame(width: 339.0, height: 241.0)
-                Spacer()
-                    .frame(height: 2.0)
-                Text("Share Location!")
-                    .font(.custom("Ramaraja", size: 30))
-                    .multilineTextAlignment(.leading)
-                    .frame(width: 300, height: 55, alignment: .bottomLeading)
-                // [Learn more about this requirement.](https://www.apple.com)
-                Text("This app collects location services to understand the congestion distribution. By enabling the location, you are providing access to Georgia Tech researchers to use the data for future studies. You can opt out of the study at any time.")
-                    .font(.custom("Ramaraja", size: 20))
-                    .multilineTextAlignment(.leading)
-                    .frame(width: 300, height: 250, alignment: .leading)
-                Button(action: {
-                    UIApplication.shared.open(URL(string: UIApplication.openSettingsURLString)!)
-                }, label: {
-                    Text("Enable Location Always")
-                        .foregroundColor(.white)
-                        .font(.custom("Ramaraja", size: 20))
-                        .multilineTextAlignment(.center)
-                        .background(Rectangle()
-                                        .fill(Color("UiGreen"))
-                                        .frame(width: 300, height: 64))
-                })
-                    .padding(5.0)
-                Spacer()
-                    .frame(height: 20.0)
-                HStack(spacing: 106) {
-                    Image("PeachTreeCornersLogo")
+        NavigationView {
+            ZStack {
+                Color.white.edgesIgnoringSafeArea(.all)
+                VStack {
+                    Image("NotifTop image")
                         .resizable()
-                        .frame(width: 80.0, height: 108.0)
-                    Image("GTLogo")
-                        .resizable()
-                        .frame(width: 111.0, height: 111.0)
+                        .frame(width: 339.0, height: 241.0)
+                    Spacer()
+                        .frame(height: 2.0)
+                    Text("Share Location!")
+                        .font(.custom("Ramaraja", size: 30))
+                        .multilineTextAlignment(.leading)
+                        .frame(width: 300, height: 55, alignment: .bottomLeading)
+                    // [Learn more about this requirement.](https://www.apple.com)
+                    Text("This app collects location services to understand the congestion distribution. By enabling the location, you are providing access to Georgia Tech researchers to use the data for future studies. You can opt out of the study at any time.")
+                        .font(.custom("Ramaraja", size: 19))
+                        .multilineTextAlignment(.leading)
+                        .frame(width: 300, height: 200, alignment: .topLeading)
+                    Button(action: {
+                        UIApplication.shared.open(URL(string: UIApplication.openSettingsURLString)!)
+                    }, label: {
+                        Text("Enable Location Always")
+                            .foregroundColor(.white)
+                            .font(.custom("Ramaraja", size: 20))
+                            .multilineTextAlignment(.center)
+                            .background(Rectangle()
+                                            .fill(Color("UiGreen"))
+                                            .frame(width: 300, height: 55))
+                    })
+                        .padding(2.0)
+                    Spacer()
+                        .frame(height: 28.0)
+                    NavigationLink(destination: RequestPushNotifView()) {
+                        Text("Skip for now")
+                            .font(.custom("Ramaraja", size: 18))
+                            .foregroundColor(Color.init(UIColor.gray))
+                    }.padding(-8)
+                    HStack(spacing: 140) {
+                        Image("PeachTreeCornersLogo")
+                            .resizable()
+                            .frame(width: 70.0, height: 94.5)
+                        Image("GTLogo")
+                            .resizable()
+                            .frame(width: 90.0, height: 90.0)
+                    }
                 }
             }
-        }
+        }.navigationBarHidden(true)
     }
 }
 
