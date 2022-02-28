991b97f9326f94874dc3f088942ddab66dc5cdf9
Shuangyue Cheng <kat@Shuangyues-MacBook-Pro.local>
2022-02-28 12:20:30 -0600

    add scrollview and change font size for smaller screens compatibility ie-iphone8

--- VIPapplication/View/RequestLocationNotifView.swift
+++ VIPapplication/View/RequestLocationNotifView.swift
@@ -14,60 +14,63 @@ struct RequestLocationNotifView: View {
     
     var body: some View {
         NavigationView {
-            ZStack {
-                
-                VStack {
-                    Image("LocationSharing")
-                        .resizable()
-                        .frame(width: 339.0, height: 241.0)
-                    
-                    Spacer()
-                        .frame(height: 2.0)
-                    
-                    Text("Share Location!")
-                        .font(.custom("Ramaraja", size: 30))
-                        .multilineTextAlignment(.leading)
-                        .frame(width: 300, height: 55, alignment: .bottomLeading)
-                    // [Learn more about this requirement.](https://www.apple.com)
-                    Text("This app collects location services to understand the congestion distribution. By enabling the location, you are providing access to Georgia Tech researchers to use the data for future studies. You can opt out of the study at any time.")
-                        .font(.custom("Ramaraja", size: 19))
-                        .multilineTextAlignment(.leading)
-                        .frame(width: 300, height: 200, alignment: .topLeading)
-                    
-                    Button(action: {
-                        UIApplication.shared.open(URL(string: UIApplication.openSettingsURLString)!)
-                    }, label: {
-                        Text("Enable Location Always")
-                            .foregroundColor(.white)
-                            .font(.custom("Ramaraja", size: 20))
-                            .multilineTextAlignment(.center)
-                            .background(Rectangle()
-                                            .fill(Color("UiGreen"))
-                                            .frame(width: 300, height: 55))
-                    })
-                        .padding(2.0)
-                    
-                    Spacer()
-                        .frame(height: 28.0)
-                    
-                    NavigationLink(destination: RequestPushNotifView()) {
-                        Text("Skip for now")
-                            .font(.custom("Ramaraja", size: 18))
-                            .foregroundColor(Color.init(UIColor.gray))
-                    }.padding(-8)
-                    
-                    HStack(spacing: 140) {
-                        Image("PeachTreeCornersLogo")
+            ScrollView(showsIndicators: false) {
+                ZStack {
+                    VStack {
+                        Spacer()
+                            .frame(height: 25.0)
+                        Image("LocationSharing")
                             .resizable()
-                            .frame(width: 70.0, height: 94.5)
-                        Image("GTLogo")
-                            .resizable()
-                            .frame(width: 90.0, height: 90.0)
+                            .frame(width: 339.0, height: 241.0)
+                        
+                        Spacer()
+                            .frame(height: 20.0)
+                        
+                        Text("Share Location!")
+                            .font(.custom("Ramaraja", size: 30))
+                            .multilineTextAlignment(.leading)
+                            .frame(width: 300, height: 55, alignment: .bottomLeading)
+                        // [Learn more about this requirement.](https://www.apple.com)
+                        Text("This app collects location services to understand the congestion distribution. By enabling the location, you are providing access to Georgia Tech researchers to use the data for future studies. You can opt out of the study at any time.")
+                            .font(.custom("Ramaraja", size: 19))
+                            .multilineTextAlignment(.leading)
+                            .frame(width: 300, height: 200, alignment: .topLeading)
+                        
+                        Button(action: {
+                            UIApplication.shared.open(URL(string: UIApplication.openSettingsURLString)!)
+                        }, label: {
+                            Text("Enable Location Always")
+                                .foregroundColor(.white)
+                                .font(.custom("Ramaraja", size: 20))
+                                .multilineTextAlignment(.center)
+                                .background(Rectangle()
+                                                .fill(Color("UiGreen"))
+                                                .frame(width: 300, height: 55))
+                        })
+                            .padding(2.0)
+                        
+                        Spacer()
+                            .frame(height: 28.0)
+                        
+                        NavigationLink(destination: RequestPushNotifView()) {
+                            Text("Skip for now")
+                                .font(.custom("Ramaraja", size: 18))
+                                .foregroundColor(Color.init(UIColor.gray))
+                        }.padding(-8)
+                        
+                        HStack(spacing: 140) {
+                            Image("PeachTreeCornersLogo")
+                                .resizable()
+                                .frame(width: 70.0, height: 94.5)
+                            Image("GTLogo")
+                                .resizable()
+                                .frame(width: 90.0, height: 90.0)
+                        }
                     }
                 }
-            }
+            }.navigationBarHidden(true)
+                .environmentObject(user)
         }.navigationBarHidden(true)
-            .environmentObject(user)
     }
 }
 
--- VIPapplication/View/RequestPushNotifView.swift
+++ VIPapplication/View/RequestPushNotifView.swift
@@ -13,47 +13,53 @@ struct RequestPushNotifView: View {
     
     var body: some View {
         NavigationView {
-            ZStack {
-                VStack {
-                    Image("LocationSharing")
-                        .resizable()
-                        .frame(width: 339.0, height: 241.0)
-                    
-                    Spacer()
-                        .frame(height: 50.0)
-                    
-                    Text("Don't miss out!")
-                        .font(.custom("Ramaraja", size: 30))
-                        .multilineTextAlignment(.leading)
-                        .frame(width: 300, height: 55, alignment: .bottomLeading)
-                    
-                    Text("Get push notifications for available incentives, research updates and other activities going on! [Learn more about this requirement.](https://www.apple.com)")
-                        .font(.custom("Ramaraja", size: 20))
-                        .multilineTextAlignment(.leading)
-                        .frame(width: 300, height: 140, alignment: .leading)
-                    
-                    Button(action: {
-                        UIApplication.shared.open(URL(string: UIApplication.openSettingsURLString)!)
-                    }, label: {
-                        Text("Enable Push Notifications")
-                            .foregroundColor(.white)
+            ScrollView(showsIndicators: false) {
+                ZStack {
+                    VStack {
+                        Spacer()
+                            .frame(height: 55.0)
+                        Image("LocationSharing")
+                            .resizable()
+                            .frame(width: 339.0, height: 241.0)
+                        
+                        Spacer()
+                            .frame(height: 50.0)
+                        
+                        Text("Don't miss out!")
+                            .font(.custom("Ramaraja", size: 30))
+                            .multilineTextAlignment(.leading)
+                            .frame(width: 300, height: 55, alignment: .bottomLeading)
+                        
+                        Text("Get push notifications for available incentives, research updates and other activities going on! [Learn more about this requirement.](https://www.apple.com)")
                             .font(.custom("Ramaraja", size: 20))
-                            .multilineTextAlignment(.center)
-                            .background(Rectangle()
-                                            .fill(Color("UiGreen"))
-                                            .frame(width: 300, height: 64))
-                    })
-                        .padding(40.0)
-                    
-                    NavigationLink(destination: MapBoxMapView()) {
-                        Text("Skip for now")
-                            .font(.custom("Ramaraja", size: 18))
-                            .foregroundColor(Color.init(UIColor.gray))
-                    }.padding(-20)
+                            .multilineTextAlignment(.leading)
+                            .frame(width: 300, height: 140, alignment: .leading)
+                        
+                        Button(action: {
+                            UIApplication.shared.open(URL(string: UIApplication.openSettingsURLString)!)
+                        }, label: {
+                            Text("Enable Push Notifications")
+                                .foregroundColor(.white)
+                                .font(.custom("Ramaraja", size: 20))
+                                .multilineTextAlignment(.center)
+                                .background(Rectangle()
+                                                .fill(Color("UiGreen"))
+                                                .frame(width: 300, height: 64))
+                        })
+                            .padding(40.0)
+                        
+                        NavigationLink(destination: MapBoxMapView()) {
+                            Text("Skip for now")
+                                .font(.custom("Ramaraja", size: 18))
+                                .foregroundColor(Color.init(UIColor.gray))
+                        }.padding(-20)
+                        Spacer()
+                            .frame(height: 20.0)
+                    }
                 }
-            }
+            }.navigationBarHidden(true)
+                .environmentObject(user)
         }.navigationBarHidden(true)
-            .environmentObject(user)
     }
 }
 
