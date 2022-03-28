c4724068bf0b2c6576c422b7980f12eec88bc9fb
Meikle, Braeden Andrew <bmeikle3@gatech.edu>
2022-03-28 12:49:38 -0400

    Fix bug in VIPapplicationTests.swift for isValidEmailAddress function call

--- VIPapplicationTests/VIPapplicationTests.swift
+++ VIPapplicationTests/VIPapplicationTests.swift
@@ -28,26 +28,26 @@ class VIPapplicationTests: XCTestCase {
     
     /* test that tries to sign up with a bad email address */
     func testSignUpBadEmail() throws {
-        XCTAssertTrue(SignUpView.isValidEmailAddress(email: "failure@domain.c"))
-        XCTAssertTrue(SignUpView.isValidEmailAddress(email: "whoops@@doubleatsign.edu"))
-        XCTAssertTrue(SignUpView.isValidEmailAddress(email: "s(oory)@gmail.com"))
-        XCTAssertTrue(SignUpView.isValidEmailAddress(email: "another_failure..twoconsecutivedots@testing.org"))
-        XCTAssertTrue(SignUpView.isValidEmailAddress(email: "missing@end"))
-        XCTAssertTrue(SignUpView.isValidEmailAddress(email: "alsobad@z.z"))
-        XCTAssertTrue(SignUpView.isValidEmailAddress(email: "nice try@gatech.edu"))
-        XCTAssertTrue(SignUpView.isValidEmailAddress(email: "withoutthe.@gatechedu"))
-        XCTAssertTrue(SignUpView.isValidEmailAddress(email: "notokay#brae@yahoo.mail"))
-        XCTAssertTrue(SignUpView.isValidEmailAddress(email: "withouttheatgatech.edu"))
+        XCTAssertTrue(LoginManager.isValidEmailAddress(email: "failure@domain.c"))
+        XCTAssertTrue(LoginManager.isValidEmailAddress(email: "whoops@@doubleatsign.edu"))
+        XCTAssertTrue(LoginManager.isValidEmailAddress(email: "s(oory)@gmail.com"))
+        XCTAssertTrue(LoginManager.isValidEmailAddress(email: "another_failure..twoconsecutivedots@testing.org"))
+        XCTAssertTrue(LoginManager.isValidEmailAddress(email: "missing@end"))
+        XCTAssertTrue(LoginManager.isValidEmailAddress(email: "alsobad@z.z"))
+        XCTAssertTrue(LoginManager.isValidEmailAddress(email: "nice try@gatech.edu"))
+        XCTAssertTrue(LoginManager.isValidEmailAddress(email: "withoutthe.@gatechedu"))
+        XCTAssertTrue(LoginManager.isValidEmailAddress(email: "notokay#brae@yahoo.mail"))
+        XCTAssertTrue(LoginManager.isValidEmailAddress(email: "withouttheatgatech.edu"))
     }
     
     /* tests successfully signing up with a good email address */
     func testSignUpGoodEmail() throws {
-        XCTAssertFalse(SignUpView.isValidEmailAddress(email: "validemail1234567890@valid.com"))
-        XCTAssertFalse(SignUpView.isValidEmailAddress(email: "this.email.address.also.works@does-not.matter.com"))
-        XCTAssertFalse(SignUpView.isValidEmailAddress(email: "another-fine-address@alittlebitlongbutitisfine.uk"))
-        XCTAssertFalse(SignUpView.isValidEmailAddress(email: "123.fine.123.123@abc.com"))
-        XCTAssertFalse(SignUpView.isValidEmailAddress(email: "upperCASEworksTOO@ABCDHSS.ABDBSSS.com"))
-        XCTAssertFalse(SignUpView.isValidEmailAddress(email: "werexow661@jo6s.com"))
+        XCTAssertFalse(LoginManager.isValidEmailAddress(email: "validemail1234567890@valid.com"))
+        XCTAssertFalse(LoginManager.isValidEmailAddress(email: "this.email.address.also.works@does-not.matter.com"))
+        XCTAssertFalse(LoginManager.isValidEmailAddress(email: "another-fine-address@alittlebitlongbutitisfine.uk"))
+        XCTAssertFalse(LoginManager.isValidEmailAddress(email: "123.fine.123.123@abc.com"))
+        XCTAssertFalse(LoginManager.isValidEmailAddress(email: "upperCASEworksTOO@ABCDHSS.ABDBSSS.com"))
+        XCTAssertFalse(LoginManager.isValidEmailAddress(email: "werexow661@jo6s.com"))
     }
 
     func testPerformanceExample() throws {
