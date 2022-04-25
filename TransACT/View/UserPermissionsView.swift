2a70a6ebcc20c7f2dec5ebcce4f8959a391137fa
Meikle, Braeden Andrew <bmeikle3@gatech.edu>
2022-04-25 14:15:47 -0400

    Try to fix location and notification request buttons, no success

--- TransACT/View/UserPermissionsView.swift
+++ TransACT/View/UserPermissionsView.swift
@@ -10,18 +10,18 @@ import SwiftUI
 struct UserPermissionsView: View {
     
     @EnvironmentObject var user: User
-    //@ObservedObject var locationManager = LocationManager.shared
-    //@ObservedObject var notificationManager = NotificationManager.shared
-    //@AppStorage("NotificationPushInitialRequest") var initialRequestDone: Bool = false
+    @ObservedObject var locationManager = LocationManager.shared
+    @ObservedObject var notificationManager = NotificationManager.shared
+    
     @State private var showPopup: Bool = false
-    @State private var enabledPushNotif: Bool = false
-    @State private var enabledLocation: Bool = false
+    //@State private var initLocReq: Bool = false
+    //@AppStorage("NotificationPushInitialRequest") var initNotifReq: Bool = false
     
     /*func passUserToLocationManager() {
         locationManager.setUser(user: user)
     }*/
     
-    func requestPushNotif() {
+    /*func requestPushNotif() {
         let center = UNUserNotificationCenter.current()
         center.requestAuthorization(options: [.alert, .sound, .badge]) { granted, error in
             
@@ -32,23 +32,22 @@ struct UserPermissionsView: View {
             enabledPushNotif = true
             // Enable or disable features based on the authorization.
         }
-    }
+    }*/
     
     var body: some View {
-        
-        /*if locationManager.userLocation != nil {
-            // if user denied -> userLocation is nil
-            UserPermissionsView().task {
-                passUserToLocationManager()
-            }
-        }*/
-        
         VStack {
             
-            if enabledLocation && enabledPushNotif {
-                // go to MainTabView
+            /*if initLocReq && initNotifReq {
+                // asked for permissions, go to MainTabView
                 NavigationLink(destination: MainTabView(), label: {})
-            }
+            }*/
+            
+            /*if locationManager.userLocation != nil {
+                // if user denied -> userLocation is nil
+                UserPermissionsView().task {
+                    passUserToLocationManager()
+                }
+            }*/
             
             Spacer().frame(height: 30)
             Text("Share location data and allow push notifications")
@@ -57,19 +56,17 @@ struct UserPermissionsView: View {
             Spacer().frame(height: 30)
             Group {
                 Button(action: {
-                    //locationManager.requestLocation()
+                    locationManager.requestLocation()
                     //passUserToLocationManager()
+                    //initLocReq = true
+                    
                     // UIApplication.shared.open(URL(string: UIApplication.openSettingsURLString)!)
                     /*
-                     
                      for location:
                      https://developer.apple.com/documentation/corelocation/requesting_authorization_for_location_services
                      ->NSLocationWhenInUsageDescription Key
                      https://developer.apple.com/documentation/bundleresources/information_property_list/nslocationwheninuseusagedescription
                      https://stackoverflow.com/questions/46193797/nslocationwheninuseusagedescription-warning-but-i-have-already-added-it
-                     
-                     
-                     
                      */
                 }, label: {
                     ZStack {
@@ -82,10 +79,9 @@ struct UserPermissionsView: View {
                     }
                 })
                 Button(action: {
-                    //notificationManager.requestAuthorization()
-                    //initialRequestDone = true
-                    // UIApplication.shared.open(URL(string: UIApplication.openSettingsURLString)!)
-                    requestPushNotif()
+                    notificationManager.requestAuthorization()
+                    //initNotifReq = true
+                    //requestPushNotif()
                 }, label: {
                     ZStack {
                         Rectangle()
@@ -122,10 +118,6 @@ struct UserPermissionsView: View {
                     .resizable()
                     .frame(width: 90.0, height: 90.0)
             }
-            
-            /*if initialRequestDone {
-                NavigationLink(destination: MainTabView(), label: {})
-            }*/
         }
             .navigationBarHidden(true)
             .environmentObject(user)
@@ -133,13 +125,11 @@ struct UserPermissionsView: View {
 }
 
 struct PopupView: View {
-    
     var body: some View {
         VStack {
             Image("LocationSharing")
                 .resizable()
                 .frame(width: 300, height: 200)
-            // [Learn more about this requirement.](https://www.apple.com)
             Text("This app collects location services to understand the congestion distribution. By enabling the location, you are providing access to Georgia Tech researchers to use the data for future studies. You can opt out of the study at any time.")
                 .font(.custom("Ramaraja", size: 20))
                 .multilineTextAlignment(.leading)
@@ -147,7 +137,7 @@ struct PopupView: View {
             Text("Get push notifications for available incentives, research updates and other activities going on! [Learn more about this requirement.](https://www.apple.com)")
                 .font(.custom("Ramaraja", size: 20))
                 .multilineTextAlignment(.leading)
-                .frame(width: 300, height: 140, alignment: .leading)
+                .frame(width: 300, height: 200, alignment: .topLeading)
         }
     }
 }
