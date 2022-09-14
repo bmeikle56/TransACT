ceabf397dba20076a6c6ebb5bcaae43dbaed1b02
Meikle, Braeden Andrew <bmeikle3@gatech.edu>
2022-09-13 20:40:58 -0400

    Add code to create Map, default region in MapView

--- TransACT/View/MapView.swift
+++ TransACT/View/MapView.swift
@@ -9,8 +9,20 @@ import SwiftUI
 import MapKit
 
 struct MapView: View {
+    
+    // random, copied from online, change later
+    @State private var defaultRegion = MKCoordinateRegion(
+        center: CLLocationCoordinate2D(
+            latitude: 40.83834587046632,
+            longitude: 14.254053016537693),
+        span: MKCoordinateSpan(
+            latitudeDelta: 0.03,
+            longitudeDelta: 0.03)
+        )
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        Map(coordinateRegion: $defaultRegion)
+            .edgesIgnoringSafeArea(.all)
     }
 }
 
